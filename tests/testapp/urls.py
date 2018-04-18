b920510f6715d1c7ca6513a664e9e50dbc7186bb
Matthias Kestenholz <mk@feinheit.ch>
2018-04-18 12:03:46 +0200

    path() is not available on Django 1.11

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,7 +1,7 @@
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
