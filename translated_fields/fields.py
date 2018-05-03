1f020994f8196fcb23239634da88e2b51e4d0ed8
Matthias Kestenholz <mk@feinheit.ch>
2018-05-03 13:32:36 +0200

    Reformat/refactor

--- translated_fields/fields.py
+++ translated_fields/fields.py
@@ -3,38 +3,33 @@ import re
 from django.conf import settings
 from django.db.models import Field
 from django.utils.functional import keep_lazy_text
-from django.utils.module_loading import import_string
 from django.utils.translation import get_language
 
 
 __all__ = (
-    'TranslatedField',
-    'to_attribute',
-    'translated_attrgetter',
-    'translated_attrsetter',
-    'translated_attributes',
+    'TranslatedField', 'to_attribute', 'translated_attrgetter',
+    'translated_attrsetter', 'translated_attributes',
 )
 
 
-def to_attribute(name, language):
+def to_attribute(name, language_code=None):
+    language = language_code or get_language()
     return re.sub(r'[^a-z0-9_]+', '_', ('%s_%s' % (name, language)).lower())
 
 
 def translated_attrgetter(name):
-    return lambda self: getattr(self, to_attribute(name, get_language()))
+    return lambda self: getattr(self, to_attribute(name))
 
 
 def translated_attrsetter(name):
-    return lambda self, value:\
-        setattr(self, to_attribute(name, get_language()), value)
+    return lambda self, value: setattr(self, to_attribute(name), value)
 
 
-def translated_attributes(*names):
+def translated_attributes(*names, attrgetter=translated_attrgetter):
     def decorator(cls):
         for name in names:
-            setattr(cls, name, property(translated_attrgetter(name)))
+            setattr(cls, name, property(attrgetter(name)))
         return cls
-
     return decorator
 
 
@@ -44,22 +39,14 @@ def verbose_name_with_language(verbose_name, language_code):
 
 
 class TranslatedField(object):
-    def __init__(
-            self,
-            field,
-            specific=None,
-            *,
-            verbose_name_with_language=True,
-            languages=None,
-            attrgetter=None,
-            attrsetter=None
-    ):
+    def __init__(self, field, specific=None, *,
+                 verbose_name_with_language=True,
+                 languages=None, attrgetter=None, attrsetter=None):
         self._field = field
         self._specific = specific or {}
         self._verbose_name_with_language = verbose_name_with_language
         self._attrgetter = attrgetter or translated_attrgetter
         self._attrsetter = attrsetter or translated_attrsetter
-
         self.languages = list(languages or (l[0] for l in settings.LANGUAGES))
 
         # Make space for our fields. Can be removed when dropping support
@@ -68,13 +55,11 @@ class TranslatedField(object):
         Field.creation_counter += len(settings.LANGUAGES)
 
     def contribute_to_class(self, cls, name):
-        _n, path, args, kwargs = self._field.deconstruct()
+        _n, _p, args, kwargs = self._field.deconstruct()
         verbose_name = kwargs.pop('verbose_name', name)
-        field = import_string(path)
         fields = []
-
         for language_code in self.languages:
-            f = field(
+            f = self._field.__class__(
                 verbose_name=verbose_name_with_language(
                     verbose_name,
                     language_code,
