993fba6e0e979911c9b04adbd34bdf4312770033
Matthias Kestenholz <mk@feinheit.ch>
2020-05-14 19:20:29 +0200

    Stop hiding import errors
    Fixes #21

--- translated_fields/__init__.py
+++ translated_fields/__init__.py
@@ -2,8 +2,10 @@ VERSION = (0, 9, 0)
 __version__ = ".".join(map(str, VERSION))
 
 try:
+    import django
+except ModuleNotFoundError:  # pragma: no cover
+    pass
+else:
     from .admin import *  # noqa
     from .fields import *  # noqa
     from .utils import *  # noqa
-except ImportError:  # pragma: no cover
-    pass
