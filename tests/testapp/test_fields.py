3125a13a694cdb40c7685fb13e3a807e887f5e0d
Matthias Kestenholz <mk@feinheit.ch>
2025-01-31 09:09:03 +0100

    Convert the test suite to pytest

--- tests/testapp/test_fields.py
+++ tests/testapp/test_fields.py
@@ -1,14 +1,15 @@
 import re
-from unittest import skipIf
 
 import django
+import pytest
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
-from django.forms.models import modelform_factory
-from django.test import Client, TestCase
-from django.utils.translation import deactivate_all, override
+from django.forms import modelform_factory
+from django.test import Client
+from django.utils.translation import override
+from pytest_django.asserts import assertInHTML
 
 from testapp.models import (
     CustomLanguagesModel,
@@ -18,267 +19,300 @@ from testapp.models import (
     SpecificModel,
     TestModel,
 )
-from translated_fields.utils import language_code_formfield_callback
+from translated_fields import language_code_formfield_callback
 
 
-class Test(TestCase):
-    def setUp(self):
-        deactivate_all()
+@pytest.fixture
+def user(db):
+    return User.objects.create_superuser("admin", "admin@test.ch", "blabla")
 
-    def login(self):
-        self.user = User.objects.create_superuser("admin", "admin@test.ch", "blabla")
-        client = Client()
-        client.force_login(self.user)
-        return client
 
-    def test_meta(self):
-        self.assertEqual(
-            [f.name for f in TestModel._meta.get_fields()],
-            ["id", "name_en", "name_de", "other_en", "other_de"],
-        )
+@pytest.fixture
+def login(db, user):
+    client = Client()
+    client.force_login(user)
+    return client
 
-        creation_counters = [
-            TestModel._meta.get_field(field).creation_counter
-            for field in ["name_en", "name_de", "other_en", "other_de"]
-        ]
-        self.assertEqual(creation_counters, sorted(creation_counters))
 
-        other_en = TestModel._meta.get_field("other_en")
-        other_de = TestModel._meta.get_field("other_de")
+@pytest.mark.django_db
+def test_meta():
+    assert [f.name for f in TestModel._meta.get_fields()] == [
+        "id",
+        "name_en",
+        "name_de",
+        "other_en",
+        "other_de",
+    ]
 
-        with override("en"):
-            self.assertEqual(str(other_en.verbose_name), "other field")
-            self.assertEqual(str(other_de.verbose_name), "other field")
+    creation_counters = [
+        TestModel._meta.get_field(field).creation_counter
+        for field in ["name_en", "name_de", "other_en", "other_de"]
+    ]
+    assert creation_counters == sorted(creation_counters)
 
-        with override("de"):
-            self.assertEqual(str(other_en.verbose_name), "anderes Feld")
-            self.assertEqual(str(other_de.verbose_name), "anderes Feld")
+    other_en = TestModel._meta.get_field("other_en")
+    other_de = TestModel._meta.get_field("other_de")
 
-    def test_form_field_order(self):
-        form = modelform_factory(TestModel, fields="__all__")()
-        self.assertEqual(
-            list(form.fields), ["name_en", "name_de", "other_en", "other_de"]
-        )
+    with override("en"):
+        assert str(other_en.verbose_name) == "other field"
+        assert str(other_de.verbose_name) == "other field"
 
-    def test_translated_fields(self):
-        m = TestModel()
-        with override(None):
-            self.assertEqual(m.name, m.name_en)
-        with override("en"):
-            self.assertEqual(m.name, m.name_en)
-        with override("de"):
-            self.assertEqual(m.name, m.name_de)
-        with override("bla"):
-            self.assertRaises(AttributeError, lambda: m.name)
+    with override("de"):
+        assert str(other_en.verbose_name) == "anderes Feld"
+        assert str(other_de.verbose_name) == "anderes Feld"
 
-    def test_translated_attributes(self):
-        m = TestModel()
-        with override(None):
-            self.assertEqual(m.stuff, m.stuff_en)
-        with override("en"):
-            self.assertEqual(m.stuff, m.stuff_en)
-        with override("de"):
-            self.assertEqual(m.stuff, m.stuff_de)
-        with override("bla"):
-            self.assertRaises(AttributeError, lambda: m.stuff)
 
-    def test_translated_attributes_with_fallback_to_default(self):
-        m = TestModel()
-        with override("de"):
-            self.assertEqual(m.attr_default, "en")
+@pytest.mark.django_db
+def test_form_field_order():
+    form = modelform_factory(TestModel, fields="__all__")()
+    assert list(form.fields) == ["name_en", "name_de", "other_en", "other_de"]
 
-    def test_translated_attributes_with_fallback_to_any(self):
-        m = TestModel()
-        with override("de"):
-            self.assertEqual(m.attr_any, "en")
 
-    def test_admin_changelist_redirect(self):
-        """Redirects have no render() method, TranslatedFieldAdmin shouldn't crash"""
-        client = self.login()
-        response = client.get("/admin/testapp/testmodel/?hello=world")
-        self.assertRedirects(response, "/admin/testapp/testmodel/?e=1")
+@pytest.mark.django_db
+def test_translated_fields():
+    m = TestModel()
+    with override(None):
+        assert m.name == m.name_en
+    with override("en"):
+        assert m.name == m.name_en
+    with override("de"):
+        assert m.name == m.name_de
+    with override("bla"):
+        with pytest.raises(AttributeError):
+            _ = m.name
 
-    def test_admin(self):
-        client = self.login()
-        response = client.get("/admin/testapp/testmodel/add/")
-        self.assertContains(response, "Name [de]")
-        self.assertContains(response, "Other field [en]")
 
-        m = TestModel.objects.create(name_en="Test")
+@pytest.mark.django_db
+def test_translated_attributes():
+    m = TestModel()
+    with override(None):
+        assert m.stuff == m.stuff_en
+    with override("en"):
+        assert m.stuff == m.stuff_en
+    with override("de"):
+        assert m.stuff == m.stuff_de
+    with override("bla"):
+        with pytest.raises(AttributeError):
+            _ = m.stuff
 
-        response = client.get("/admin/testapp/testmodel/")
-        self.assertContains(response, "<span>Name</span>")
-        self.assertContains(response, "<span>Other field</span>")
 
-        self.assertContains(response, "Name [en]</a>")
-        self.assertContains(response, "Name [de]</a>")
+@pytest.mark.django_db
+def test_translated_attributes_with_fallback_to_default():
+    m = ModelWithFallback()
+    with override(None):
+        assert m.required == m.required_en
+    with override("en"):
+        assert m.required == m.required_en
+    with override("de"):
+        assert m.required == m.required_en
 
-        self.assertContains(
-            response,
-            '<th class="field-name_en"><a'
-            f' href="/admin/testapp/testmodel/{m.id}/change/">Test</a></th>',
-            html=True,
-        )
-        self.assertContains(response, 'id="id_form-0-name_de"')
 
-        response = client.get(f"/admin/testapp/testmodel/{m.id}/change/")
-        self.assertContains(
-            response,
-            (
-                '<label>Other field [en]:</label><div class="readonly"></div>'
-                if django.VERSION < (5, 1)
-                else '<label>Other field [en]:</label><div class="readonly">-</div>'
-            ),
-            html=True,
-        )
+@pytest.mark.django_db
+def test_translated_attributes_with_fallback_to_any():
+    m = ModelWithAnyFallback()
+    with override(None):
+        assert m.optional == ""
+    with override("en"):
+        assert m.optional == ""
+    with override("de"):
+        assert m.optional == ""
 
-        response = client.post(
-            "/admin/testapp/testmodel/add/", {"name_en": "Test", "name_de": "Test"}
-        )
-        self.assertRedirects(response, "/admin/testapp/testmodel/")
 
-        # print(response, response.content.decode("utf-8"))
+@pytest.mark.django_db
+def test_admin_changelist_redirect(login):
+    client = login
+    response = client.get("/admin/testapp/testmodel/?name=bla")
+    assert response.status_code == 302
 
-    def test_custom_languages(self):
-        m = CustomLanguagesModel()
 
-        self.assertEqual(m.name, "NO VALUE")
-        m.name_it = "it"
-        self.assertEqual(m.name, "it")
-        m.name_fr = "fr"
-        self.assertEqual(m.name, "fr")
-        m.name_it = ""
-        self.assertEqual(m.name, "fr")
+@pytest.mark.django_db
+def test_admin(login):
+    client = login
+    response = client.get("/admin/testapp/testmodel/add/")
+    assert "Name [de]" in response.content.decode()
+    assert "Other field [en]" in response.content.decode()
 
-        # The attributes from LANGUAGES should not exist:
-        with self.assertRaises(AttributeError):
-            _read = m.name_en
-        with self.assertRaises(AttributeError):
-            _read = m.name_de
+    m = TestModel.objects.create(name_en="Test")
 
-    def test_translated_field_instance(self):
-        self.assertEqual(CustomLanguagesModel.name.languages, ["fr", "it"])
-        m = CustomLanguagesModel()
-        with self.assertRaises(AttributeError):
-            _read = m.name.languages
+    response = client.get("/admin/testapp/testmodel/")
+    assert "<span>Name</span>" in response.content.decode()
+    assert "<span>Other field</span>" in response.content.decode()
 
-        self.assertEqual(m.__class__.name.languages, ["fr", "it"])
+    assert "Name [en]</a>" in response.content.decode()
+    assert "Name [de]</a>" in response.content.decode()
 
-        self.assertEqual(CustomLanguagesModel.name.fields, ["name_fr", "name_it"])
+    assert (
+        f'<th class="field-name_en"><a href="/admin/testapp/testmodel/{m.id}/change/">Test</a></th>'
+        in response.content.decode()
+    )
+    assert 'id="id_form-0-name_de"' in response.content.decode()
 
-        # Not str, lazy!
-        self.assertFalse(CustomLanguagesModel.name.short_description is str)
+    response = client.get(f"/admin/testapp/testmodel/{m.id}/change/")
+    expected_content = (
+        '<label>Other field [en]:</label><div class="readonly"></div>'
+        if django.VERSION < (5, 1)
+        else '<label>Other field [en]:</label><div class="readonly">-</div>'
+    )
+    assertInHTML(expected_content, response.content.decode())
 
-    def test_specific(self):
-        m = SpecificModel()
+    response = client.post(
+        "/admin/testapp/testmodel/add/", {"name_en": "Test", "name_de": "Test"}
+    )
+    assert response.status_code == 302
+    assert response.url == "/admin/testapp/testmodel/"
 
-        with self.assertRaises(ValidationError) as cm:
-            m.full_clean()
 
-        self.assertEqual(list(cm.exception.error_dict), ["name_en"])
+@pytest.mark.django_db
+def test_custom_languages():
+    m = CustomLanguagesModel()
 
-        m.name_en = "bla"
+    assert m.name == "NO VALUE"
+    m.name_it = "it"
+    assert m.name == "it"
+    m.name_fr = "fr"
+    assert m.name == "fr"
+    m.name_it = ""
+    assert m.name == "fr"
+
+    with pytest.raises(AttributeError):
+        _ = m.name_en
+    with pytest.raises(AttributeError):
+        _ = m.name_de
+
+
+@pytest.mark.django_db
+def test_translated_field_instance():
+    assert CustomLanguagesModel.name.languages == ["fr", "it"]
+    m = CustomLanguagesModel()
+    with pytest.raises(AttributeError):
+        _ = m.name.languages
+
+    assert m.__class__.name.languages == ["fr", "it"]
+    assert CustomLanguagesModel.name.fields == ["name_fr", "name_it"]
+
+    # Not str, lazy!
+    assert CustomLanguagesModel.name.short_description is not str
+
+
+@pytest.mark.django_db
+def test_specific():
+    m = SpecificModel()
+
+    with pytest.raises(ValidationError) as exc:
         m.full_clean()
 
-        self.assertEqual(str(m._meta.get_field("name_en").verbose_name), "name")
-        self.assertEqual(str(m._meta.get_field("name_de").verbose_name), "Der Name")
+    assert list(exc.value.error_dict) == ["name_en"]
 
-    def test_setter(self):
-        m = TestModel()
-        with override("en"):
-            m.name = "english"
-        with override("de"):
-            m.name = "german"
-        self.assertEqual(m.name_en, "english")
-        self.assertEqual(m.name_de, "german")
+    m.name_en = "bla"
+    m.full_clean()
 
-        # I would rather not write code that prevents this...
-        with override("bla"):
-            m.name = "blub"
-        self.assertEqual(m.name_bla, "blub")
+    assert str(m._meta.get_field("name_en").verbose_name) == "name"
+    assert str(m._meta.get_field("name_de").verbose_name) == "Der Name"
 
-    def test_list_display_columns(self):
-        obj = ListDisplayModel.objects.create(
-            name_en="english",
-            name_de="german",
-            choice_en="a",
-            choice_de="b",
-            is_active_de=False,
-        )
-        obj.file_en.save("test.txt", ContentFile(b"Hello"), save=True)
 
-        client = self.login()
-        response = client.get("/admin/testapp/listdisplaymodel/")
+@pytest.mark.django_db
+def test_setter():
+    m = TestModel()
+    with override("en"):
+        m.name = "english"
+    with override("de"):
+        m.name = "german"
+    assert m.name_en == "english"
+    assert m.name_de == "german"
 
-        # print(response.content.decode("utf-8"))
+    with override("bla"):
+        m.name = "blub"
+    assert m.name_bla == "blub"
 
-        self.assertContains(response, "<span>Stuff</span>")
-        self.assertContains(response, "Name [en]")
-        self.assertContains(response, "Name [de]")
 
-        self.assertContains(response, "Andrew", 1)
-        self.assertContains(response, "Betty", 1)
-
-        self.assertContains(response, 'img src="/static/admin/img/icon-yes.svg"', 1)
-        self.assertContains(response, 'img src="/static/admin/img/icon-no.svg"', 1)
-
-        self.assertTrue(
-            re.search(
-                rb'<a href="/media/test(_\w+)?.txt">test(_\w+)?.txt</a>',
-                response.content,
-            )
-        )
-
-    def test_fallback(self):
-        self.assertFalse(ModelWithFallback._meta.get_field("required_en").blank)
-        self.assertTrue(ModelWithFallback._meta.get_field("required_de").blank)
-        self.assertTrue(ModelWithFallback._meta.get_field("optional_en").blank)
-        self.assertTrue(ModelWithFallback._meta.get_field("optional_de").blank)
-
-        obj = ModelWithFallback(required_en="bla")
-        with override("en"):
-            self.assertEqual(obj.required, "bla")
-        with override("de"):
-            self.assertEqual(obj.required, "bla")
-        with override(None):
-            self.assertEqual(obj.required, "bla")
-
-    def test_fallback_to_any(self):
-        obj = ModelWithAnyFallback(optional_de="chic")
-        with override("en"):
-            self.assertEqual(obj.optional, "chic")
-        with override("de"):
-            self.assertEqual(obj.optional, "chic")
-
-        obj.optional_en = "chic-en"
-        with override("en"):
-            self.assertEqual(obj.optional, "chic-en")
-        with override("de"):
-            self.assertEqual(obj.optional, "chic")
-
-        obj = ModelWithAnyFallback()
-        with override("en"):
-            self.assertEqual(obj.optional, "")
-        with override("de"):
-            self.assertEqual(obj.optional, "")
-
-    @skipIf(
-        django.VERSION < (4, 2),
-        "Specifying the callback wasn't officially supported before",
+@pytest.mark.django_db
+def test_list_display_columns(login):
+    obj = ListDisplayModel.objects.create(
+        name_en="english",
+        name_de="german",
+        choice_en="a",
+        choice_de="b",
+        is_active_de=False,
     )
-    def test_formfield_callback(self):
-        class Form(forms.ModelForm):
-            class Meta:
-                model = TestModel
-                fields = "__all__"  # noqa: DJ007
-                formfield_callback = language_code_formfield_callback
+    obj.file_en.save("test.txt", ContentFile(b"Hello"), save=True)
 
-        with override("en"):
-            result = str(Form())
-            self.assertIn("Other field [en]:", result)
-            self.assertIn("Other field [de]:", result)
+    client = login
+    response = client.get("/admin/testapp/listdisplaymodel/")
 
-        with override("de"):
-            result = str(Form())
-            self.assertIn("Anderes Feld [en]:", result)
-            self.assertIn("Anderes Feld [de]:", result)
+    assert "<span>Stuff</span>" in response.content.decode()
+    assert "Name [en]" in response.content.decode()
+    assert "Name [de]" in response.content.decode()
+
+    assert response.content.decode().count("Andrew") == 1
+    assert response.content.decode().count("Betty") == 1
+
+    assert (
+        response.content.decode().count('img src="/static/admin/img/icon-yes.svg"') == 1
+    )
+    assert (
+        response.content.decode().count('img src="/static/admin/img/icon-no.svg"') == 1
+    )
+
+    assert re.search(
+        rb'<a href="/media/test(_\w+)?.txt">test(_\w+)?.txt</a>', response.content
+    )
+
+
+@pytest.mark.django_db
+def test_fallback():
+    assert not ModelWithFallback._meta.get_field("required_en").blank
+    assert ModelWithFallback._meta.get_field("required_de").blank
+    assert ModelWithFallback._meta.get_field("optional_en").blank
+    assert ModelWithFallback._meta.get_field("optional_de").blank
+
+    obj = ModelWithFallback(required_en="bla")
+    with override("en"):
+        assert obj.required == "bla"
+    with override("de"):
+        assert obj.required == "bla"
+    with override(None):
+        assert obj.required == "bla"
+
+
+@pytest.mark.django_db
+def test_fallback_to_any():
+    obj = ModelWithAnyFallback(optional_de="chic")
+    with override("en"):
+        assert obj.optional == "chic"
+    with override("de"):
+        assert obj.optional == "chic"
+
+    obj.optional_en = "chic-en"
+    with override("en"):
+        assert obj.optional == "chic-en"
+    with override("de"):
+        assert obj.optional == "chic"
+
+    obj = ModelWithAnyFallback()
+    with override("en"):
+        assert obj.optional == ""
+    with override("de"):
+        assert obj.optional == ""
+
+
+@pytest.mark.skipif(
+    django.VERSION < (4, 2),
+    reason="Specifying the callback wasn't officially supported before",
+)
+@pytest.mark.django_db
+def test_formfield_callback():
+    class Form(forms.ModelForm):
+        class Meta:
+            model = TestModel
+            fields = "__all__"  # noqa: DJ007
+            formfield_callback = language_code_formfield_callback
+
+    with override("en"):
+        result = str(Form())
+        assert "Other field [en]:" in result
+        assert "Other field [de]:" in result
+
+    with override("de"):
+        result = str(Form())
+        assert "Anderes Feld [en]:" in result
+        assert "Anderes Feld [de]:" in result
